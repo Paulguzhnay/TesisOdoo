95122941c2cd14718d9b519c37276420cdb07802
Paulguzhnay <69018647+Paulguzhnay@users.noreply.github.com>
2023-04-30 21:00:12 -0500

    modulo pwaOffline

---
+++ pwaOffline/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'PWA Offline',
+    'version': '1.0',
+    'summary': 'Modulo para Point of Sale',
+    'description': 'Este modulo va a permitir usar el Point Of Sale de manera offline',
+    'depends': ['base'],
+    'data': [
+        'views/view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
