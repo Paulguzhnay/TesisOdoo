27592d89913362d3ed504046dd959f1e2f759cd2
Paulguzhnay <paul-guzhnay@hotmail.com>
2023-06-24 22:46:00 -0500

    Modulo

--- pwaOffline/models/model.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class MiModelo(models.Model):
-    _name = 'model'
-    _description = 'Este modulo va a permitir usar el Point Of Sale de manera offline'
-
-    campo1 = fields.Char('Campo 1')
-    campo2 = fields.Integer('Campo 2')
